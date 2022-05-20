5699f34391e039ddf0af9b3b8c35f8338e21e372
abdul1622 <104051272+abdul1622@users.noreply.github.com>
2022-05-20 19:01:10 +0530

    Delete calculator directory

--- calculator/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import calculator
-
-urlpatterns = [
-    path('',calculator),
-]
--- calculator/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def calculator(request):
-    list = ['1','2','3','+','4','5','6','-','7','8','9','*','0','.','/']
-    context ={'list':list}
-    return render(request,'calculator/calculator.html',context)
