9a1bee050d764784a5fb55fa7654132a12807d28
abdul1622 <104051272+abdul1622@users.noreply.github.com>
2022-05-20 19:01:46 +0530

    Delete todoapp directory

--- todoapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Event
-
-class EventForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = '__all__'
-        exclude = ['user','remainingHours']
--- todoapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import (event_view,event_update,event_delete)
-
-urlpatterns = [
-    path('',event_view),
-    path('update-event/<str:pk>/',event_update),
-    path('delete-event/<str:pk>/',event_delete)
-]
